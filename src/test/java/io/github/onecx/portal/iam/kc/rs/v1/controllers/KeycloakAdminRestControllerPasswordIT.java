d49335b52686505cfc9360a8e8f7d01798d9ad42
Andrej Petras <andrej@ajka-andrej.com>
2023-08-02 17:51:45 +0200

    feat: create onecx portal iam keycloak service (#1)

---
+++ src/test/java/io/github/onecx/portal/iam/kc/rs/v1/controllers/KeycloakAdminRestControllerPasswordIT.java
@@ -0,0 +1,8 @@
+package io.github.onecx.portal.iam.kc.rs.v1.controllers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class KeycloakAdminRestControllerPasswordIT extends KeycloakAdminRestControllerPasswordTest {
+
+}
